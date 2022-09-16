efd47ec2929b618e31b67d3b3303956a448a07dd
Johan <johanoscarmarte@gmail.com>
2022-09-16 15:09:05 -0400

    Creando JWT

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,6 @@
 import Usuario from "../models/Usuario.js"
-import generarId from "../helpers/generalId.js";
+import generarId from "../helpers/generarId.js";
+import generarJWT from "../helpers/generarJWT.js";
 
 
 const registrar = async (req, res) => {
@@ -11,8 +12,6 @@ const registrar = async (req, res) => {
         const error = new Error('Usuario ya registrado');
         return res.status(400).json({ msg: error.message });
     }
-
-
     try {
 
         const usuario = new Usuario(req.body)
@@ -25,11 +24,37 @@ const registrar = async (req, res) => {
         console.log(error);
         
     }
-
-    
-
-    
-
 };
 
-export {registrar}
+const autenticar = async (req, res) => {
+
+    const { email, password } = req.body;
+    
+    //Comprobar si el usuario existe
+    const usuario = await Usuario.findOne({email})
+    if (!usuario) {
+        const error = new Error("El Usuario no existe");
+        return res.status(404).json({ msg: error.message });
+    }
+    //Comproba si el usuario esta confirmado
+    if (!usuario.confimado) {
+        const error = new Error("Tu Cuenta no ha sido confirmada");
+        return res.status(403).json({ msg: error.message });
+    }
+    //Comprobar su password
+
+    if (await usuario.comprobarPassword(password)) {
+        res.json({
+            _id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email,
+            token: generarJWT(usuario._id),
+        });
+        
+    } else {
+        console.log('Es Incorrecto');
+    }
+
+}
+
+export {registrar, autenticar}
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 const router = express.Router();
 
-import {registrar } from '../controllers/usuarioController.js';
+import {registrar, autenticar } from '../controllers/usuarioController.js';
 
 //Autenticancion, Registro y Confirmacion de Usuarios
 
 router.post("/", registrar); //Crear un nuevo usuario
+router.post('/login', autenticar);
 
 
 
