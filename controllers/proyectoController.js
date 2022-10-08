0e332d0da64aea049f6303ea7a79ab92bc7674cf
Johan <johanoscarmarte@gmail.com>
2022-10-08 11:12:46 -0400

    Controladores de proyectos creado

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,6 +1,9 @@
 import Proyecto from '../models/Proyecto.js';
 
 const obtenerProyectos = async (req, res) => {
+    const proyectos = await Proyecto.find().where("creador").equals(req.usuario);
+
+    res.json(proyectos);
 
 }
 
@@ -16,21 +19,88 @@ const nuevoProyecto = async (req, res) => {
         console.log(error)
         
     }
-
-
-
     
 }
 
 const obtenerProyecto = async (req, res) => {
-    
-}
+    const { id } = req.params;
+
+    const proyecto = await Proyecto.findById(id)
+
+    if (!proyecto) {
+        const error = new Error("No Encontrado");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    //Validando que el usuario sea propietario o colaborado del proyecto
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("Acccion no valida");
+        return res.status(401).json({ msg: error.message });
+    }
+     //Validando que el usuario sea propietario o colaborado del proyecto
+
+    res.json(proyecto);
+};
 
 const editarProyecto = async (req, res) => {
-    
-}
+    const { id } = req.params;
+
+    const proyecto = await Proyecto.findById(id)
+
+    if (!proyecto) {
+        const error = new Error("No Encontrado");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    //Validando que el usuario sea propietario o colaborado del proyecto
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("Acccion no valida");
+        return res.status(401).json({ msg: error.message });
+    }
+    //Validando que el usuario sea propietario o colaborado del proyecto
+
+    proyecto.nombre = req.body.nombre || proyecto.nombre;
+    proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+    proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+    proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+    try {
+        const proyectoAlmacenado = await proyecto.save();
+        res.json(proyectoAlmacenado);
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+};
 
 const eliminarProyecto = async (req, res) => {
+    const { id } = req.params;
+
+    const proyecto = await Proyecto.findById(id)
+
+    //Validado que el proyecto exista
+    if (!proyecto) {
+        const error = new Error("No Encontrado");
+        return res.status(404).json({ msg: error.message });
+    }
+    //Validado que el proyecto exista
+
+    //Validando que el usuario sea propietario o colaborado del proyecto
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("Acccion no valida");
+        return res.status(401).json({ msg: error.message });
+    }
+    //Validando que el usuario sea propietario o colaborado del proyecto
+    try {
+        await proyecto.deleteOne();
+        res.json({ msg : "Proyecto Eliminado"})
+       
+    
+   } catch (error) {
+       console.log(error)
+    
+   }
     
 }
 
