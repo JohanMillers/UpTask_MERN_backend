782a59c6615a46834ed9e980fc54ca28ad9828b4
Johan <johanoscarmarte@gmail.com>
2023-01-07 18:53:31 -0400

    Se agregaron los populate de tareas

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -91,19 +91,21 @@ const eliminarTarea = async (req, res) => {
         return res.status(403).json({ msg: error.message });
     }
     try {
-        await tarea.deleteOne();
+        const proyecto = await Proyecto.findById(tarea.proyecto)
+        proyecto.tareas.pull(tarea._id)
+        await Promise.allSettled([await proyecto.save(), await tarea.deleteOne()]);
         res.json({msg : "Tarea elminada exitosamente"})
-        
     } catch (error) {
-        console.log(error)
-        
+        console.log(error)   
     }
 };
 
 const cambiarEstado = async (req, res) => { 
     const { id } = req.params;
 
-    const tarea = await Tarea.findById(id).populate("proyecto");
+    const tarea = await Tarea.findById(id)
+        .populate("proyecto")
+        .populate("completado");
 
     if (!tarea) {
         const error = new Error("Tarea no encontrada");
@@ -118,8 +120,14 @@ const cambiarEstado = async (req, res) => {
     }
 
     tarea.estado = !tarea.estado;
+    tarea.completado = req.usuario._id;
     await tarea.save()
-    res.json(tarea);
+
+    const tareaAlmacenada = await Tarea.findById(id)
+        .populate("proyecto")
+        .populate("completado");
+    
+    res.json(tareaAlmacenada);
 
 };
 
